e749edf8da7df3e4935fab30add49d5e33d301b5
Otávio Pace <otaviopp8@gmail.com>
2018-09-12 04:18:18 -0300

    frontend: remove semicolons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import socketIOClient from 'socket.io-client'
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   constructor() {
@@ -30,8 +30,8 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
