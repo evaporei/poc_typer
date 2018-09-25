60c607c1a2985c82d371b50ce41415f567078e01
Otávio Pace <otaviopp8@gmail.com>
2018-09-25 00:17:06 -0300

    frontend: add header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import socketIOClient from 'socket.io-client'
 import logo from './logo.svg'
 import './App.css'
+import Header from './components/Header'
 
 class App extends Component {
   constructor() {
@@ -22,13 +23,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header/>
       </div>
     )
   }
---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () =>
+  <header>
+    <h1>Poc typer</h1>
+    <hr />
+  </header>
+
+export default Header
