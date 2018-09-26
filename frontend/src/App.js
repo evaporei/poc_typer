ce34a592cae561b1156d1db5541e7321df56ed44
Otávio Pace <otaviopp8@gmail.com>
2018-09-25 22:09:51 -0300

    frontend: add basic routing for rooms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
 import socketIOClient from 'socket.io-client'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import 'antd/dist/antd.css'
+import { Spin } from 'antd'
 import logo from './logo.svg'
 import './App.css'
 import Header from './components/Header'
+
+const getRandomNumber = () =>
+  String(Math.random() * 100000000000000000)
 
 class App extends Component {
   constructor() {
@@ -21,9 +27,28 @@ class App extends Component {
   }
 
   render() {
+    const { connected } = this.state
+
     return (
-      <div className="App">
+      <div className='App'>
         <Header/>
+        <Spin tip='Connecting...' spinning={!connected} size='large'>
+          <Switch>
+            <Route
+              path='/room/:room'
+              render={props =>
+                  <Switch>
+                    <Route
+                      path={`${props.match.path}/username/:name`}
+                      render={() => <div>Game!</div>}
+                    />
+                    <Redirect to={`/room/${props.match.params.room}/username/guest_${getRandomNumber()}`}/>
+                  </Switch>
+              }
+            />
+            <Redirect to={`/room/training/username/guest_${getRandomNumber()}`}/>
+          </Switch>
+        </Spin>
       </div>
     )
   }
